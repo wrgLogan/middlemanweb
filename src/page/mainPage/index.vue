fe8477aa92a66293cf730812ffda8432b54956d1
wuronggen <wuronggen@coolcto.com>
2017-12-06 20:27:32 +0800

    页面over

--- src/page/mainPage/index.vue
+++ src/page/mainPage/index.vue
@@ -2,13 +2,66 @@
     <div id="main" class="page" @scroll="onScroll">
         <main-header></main-header>
         <div class="banner-container">
-            <div class="banner banner1"></div>
+            <img class="block" src="../../assets/image/Page1.jpg" />
+            <!-- <div class="banner banner1"></div> -->
         </div>
         <div class="banner-container">
-            <div class="banner banner2"></div>
+            <img class="block" src="../../assets/image/Page2.jpg" />
+            <!-- <div class="banner banner2"></div> -->
         </div>
         <div class="banner-container">
-            <div class="banner banner3"></div>
+            <img class="block banner3" src="../../assets/image/Page3.jpg" />    
+            <!-- <div class="banner banner3"></div> -->
+        </div>
+        <div class="banner-container">
+            <img class="block" src="../../assets/image/Page4.jpg" />        
+            <!-- <div class="banner banner4"></div> -->
+        </div>
+        <div class="banner-container">
+            <img class="block" src="../../assets/image/Page5_bg.jpg" />    
+            <img src="../../assets/image/shiyong-button.png" class="btn">        
+            <!-- <div class="banner banner5">
+                <img src="../../assets/image/shiyong-button.png" class="btn">
+            </div> -->
+        </div>
+        <div class="erweima-container">
+            <img src="../../assets/image/Page6-erweima.jpg" />
+        </div>
+        <div class="footer">
+            <ul class="about">
+                <li>
+                    <h3>合作伙伴</h3>
+                    <p>中国电信</p>
+                    <p>小米手机</p>
+                    <p>Mob.com</p>
+                </li>
+                <li>
+                    <h3>免费技术支持</h3>
+                    <p>电话：400-312-3123</p>
+                    <p>值班电话：</p>
+                    <p>【Android】188-8888-8888</p>
+                    <p>【IOS】188-8888-8888</p>
+                    <p>邮箱：support@sina.com</p>
+                </li>
+                <li>
+                    <h3>关于我们</h3>
+                    <p>公司介绍</p>
+                    <p>隐私政策</p>
+                    <p>服务条款</p>
+                    <p>联系我们</p>
+                </li>
+                <li>
+                    <h3>业务联系</h3>
+                    <p>电子邮箱：yanbandeng@sina.com</p>
+                    <p>QQ:123456789</p>
+                    <p>公司地址：</p>
+                    <p>江苏省无锡市滨湖区钱荣路68号</p>
+                    <p>邮编：123456</p>
+                </li>
+            </ul>
+            <div class="copyright">
+                CopyrightⒸ2012-2015 Dumpatiom All Rights Reserved Dumpation 版权所有 粤ICP备12079301号
+            </div>
         </div>
     </div>
 </template>
--- src/page/mainPage/script.js
+++ src/page/mainPage/script.js
@@ -9,19 +9,19 @@ export default {
     },
     methods: {
         onScroll() {
-            var banners = document.getElementsByClassName('banner');
-            var $main = document.getElementById('main');
-            console.log(banners);
-            var scrollTop = $main.scrollTop;
-            var bannerHeight = banners[0].offsetHeight;
-            var windowHeight = document.body.offsetHeight;
+            // var banners = document.getElementsByClassName('banner');
+            // var $main = document.getElementById('main');
+            // console.log(banners);
+            // var scrollTop = $main.scrollTop;
+            // var bannerHeight = banners[0].offsetHeight;
+            // var windowHeight = document.body.offsetHeight;
 
-            for (var banner of banners) {
-                console.log(banner.offsetTop - scrollTop - 66 );
-                if ((banner.offsetTop - scrollTop + 166 ) < windowHeight) {
-                    banner.setAttribute('active', '');
-                }
-            }
+            // for (var banner of banners) {
+            //     console.log(banner.offsetTop - scrollTop - 66 );
+            //     if ((banner.offsetTop - scrollTop + 166 ) < windowHeight) {
+            //         banner.setAttribute('active', '');
+            //     }
+            // }
         }
     }
 }
